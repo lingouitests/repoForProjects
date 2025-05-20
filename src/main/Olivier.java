42b2292487430b13bb5a96bceb347625ed45d7b3
lingouitests <lingouitests@gmail.com>
2025-05-20 13:50:19 +0200

    Update Olivier.java

--- src/main/Olivier.java
+++ src/main/Olivier.java
@@ -1,7 +1,7 @@
 class Olivier {
   public static void main() {
     String aaa = "aaa";
-    String bbb = "bbb_id"
+    String bbb = "bbb_id";
     System.out.println(String.format("nana %s", aaa));
   }
 }
