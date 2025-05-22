d0f228891598f75148c98893d2315d1db5a36251
lingouitests <lingouitests@gmail.com>
2025-05-22 09:54:56 +0200

    Update Olivier.java

--- src/main/Olivier.java
+++ src/main/Olivier.java
@@ -1,6 +1,12 @@
 class Olivier {
   public static void main() {
     String aaa = "aaa";
+
+
+
+
+
+    
     String ua = user + " is " + age + " years old.";
     System.out.println(String.format("nana %s", aaa));
   }
