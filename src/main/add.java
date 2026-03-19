5cf899c5c12c24a03781fcc81f2913e3baf886dc
lingouitests <lingouitests@gmail.com>
2026-03-19 12:33:19 +0100

    Create add.java

---
+++ src/main/add.java
@@ -0,0 +1,19 @@
+class Add {
+  public static void main() {
+    String bbb = "bbb";
+
+
+
+
+    
+
+//rererereryukoiloilouikyujrwawf
+    
+    String ua = user + " is " + age + " years old.";
+    System.out.println(String.format("nana %s", bbb));
+
+
+//rererererererere
+    
+  }
+}
