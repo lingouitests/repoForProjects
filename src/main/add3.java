203814bef81785c697355c34632786bcb63071b5
lingouitests <lingouitests@gmail.com>
2026-03-19 12:41:34 +0100

    Create add3.java

---
+++ src/main/add3.java
@@ -0,0 +1,19 @@
+class Add3 {
+  public static void main() {
+    String eee = "eee";
+
+
+
+
+    
+
+//rererereryukoiloilouikyujrwawf
+    
+    String ua = user + " is " + age + " years old.";
+    System.out.println(String.format("nana %s", eee));
+
+
+//rererererererere
+    
+  }
+}
