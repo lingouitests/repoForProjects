630d78dfdf29a74003f612db53118adf63b8ea4e
lingouitests <lingouitests@gmail.com>
2026-04-03 09:05:51 +0200

    Create add5.java

---
+++ src/main/add5.java
@@ -0,0 +1,19 @@
+class Add5 {
+  public static void main() {
+    String ccc = "ccc";
+
+
+
+
+    
+
+//rererereryukoiloilouikyujrwawf
+    
+    String ua = user + " is " + age + " years old.";
+    System.out.println(String.format("nana %s", ccc));
+
+
+//rererererererere
+    
+  }
+}
