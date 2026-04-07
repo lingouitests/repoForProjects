63851273f8c085545f7bfd5cbc9ed69c45acc4ee
lingouitests <lingouitests@gmail.com>
2026-04-07 08:40:20 +0200

    Update add6.java

--- src/main/add6.java
+++ src/main/add6.java
@@ -12,6 +12,7 @@ class Add6 {
     String ua = user + " is " + age + " years old.";
         String rr = users + " are " + age + " years old.";
      String tt = users + " are " + age + " years old.";
+    String ww = users + " are " + age + " years old.";
     System.out.println(String.format("nana %s", ccc));
 
 
