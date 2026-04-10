e420af6124525caa02dfb4a34e6733ff6462c6c2
lingouitests <lingouitests@gmail.com>
2026-04-10 10:14:50 +0200

    Create add5.java

---
+++ src/main/add5.java
@@ -0,0 +1,22 @@
+class Add5 {
+  public static void main() {
+    String bbb = "bbb";
+
+
+
+
+    
+
+//rererereryukoiloilouikyujrwawf
+    
+    String ua = user + " is " + age + " years old.";
+    System.out.println(String.format("nana %s", bbb));
+
+ String ee = user + " is " + age + " years old.";
+     String ee2 = user + " is " + age + " years old.";
+     String ee3 = user + " is " + age + " years old.";
+     String ee4 = user + " is " + age + " years old.";
+//rererererererere
+    
+  }
+}
