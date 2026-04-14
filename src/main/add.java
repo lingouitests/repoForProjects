da915e35e9c50e5d728877e35e70d2903f2f6cba
lingouitests <lingouitests@gmail.com>
2026-04-14 11:53:04 +0200

    Update add.java

--- src/main/add.java
+++ src/main/add.java
@@ -12,11 +12,6 @@ class Add {
     String ua = user + " is " + age + " years old.";
     System.out.println(String.format("nana %s", bbb));
 
- String ee = user + " is " + age + " years old.";
-     String ee2 = user + " is " + age + " years old.";
-     String ee3 = user + " is " + age + " years old.";
-     String ee4 = user + " is " + age + " years old.";
-     String ee5 = user + " is " + age + " years old.";
 //rererererererere
     
   }
