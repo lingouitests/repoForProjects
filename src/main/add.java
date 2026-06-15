73239a6388b7f5ca0cf281da740e8000384bff46
lingouitests <lingouitests@gmail.com>
2026-06-15 10:47:16 +0200

    Update add.java

--- src/main/add.java
+++ src/main/add.java
@@ -11,7 +11,7 @@ class Add {
     
     String ua = user + " is " + age + " years old.";
     System.out.println(String.format("nana %s", bbb));
-
+System.out.println(String.format("nana %s", bbb));
 //rererererererere
     
   }
